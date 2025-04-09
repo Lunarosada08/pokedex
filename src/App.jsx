2c858d5df50b609f4cc914a0a1af6ade61dfbc90
Renata Zavala <zamr081122mmcvrna2@lake.edu.mx>
2025-04-09 11:16:55 -0600

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+     <h1>pokedex - bulbasaur </h1>
+     <h2>Welcome to my pokedex </h2>
+     <p>here you can find information for your pokemons </p>
+
+     <div>
+      <p>Developed by Renata Zavala</p>
+      <p>2025</p>
+      <p>All rights reserved </p>
+     </div>
+    </>
+  )
+}
+
+export default App
