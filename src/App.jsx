6402c71d80cee30388a3949e9d47d99dbb7e79f1
Renata Zavala <zamr081122mmcvrna2@lake.edu.mx>
2025-04-10 09:54:49 -0600

    Seagrego la etiqueta img

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ function App() {
  
   return (
     <>
-     <h1>pokedex - bulbasaur </h1>
+     <h1>pokedex - pikschu </h1>
      <h2>Welcome to my pokedex </h2>
      <p>here you can find information for your pokemons </p>
+     <img src="" alt="" />
 
      <div>
       <p>Developed by Renata Zavala</p>
